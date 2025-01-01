4872511fc81f9f1e730da6e30ad26b31558556e6
Mohammad Nazari <mhmdnzr80.dev@gmail.com>
2025-01-01 20:46:44 +0330

    reconfig folder structure in windwos file explore

--- CharityHub.AspireHost/Program.cs
+++
@@ -1,9 +0,0 @@
-using Projects;
-
-var builder = DistributedApplication.CreateBuilder(args);
-
-// Configure application without external dependencies
-builder.AddProject<CharityHub_Endpoints>("charityhub-endpoints");
-
-// Configure application without external dependencies
-builder.Build().Run();
